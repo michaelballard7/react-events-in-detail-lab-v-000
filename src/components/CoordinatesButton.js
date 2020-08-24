8c670859fc1b76e404e2fc350f954cf6d7c346c5
Michael Ballard <ballard.michael9@gmail.com>
2020-08-24 00:58:29 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,7 +4,11 @@ import React from 'react'
 
 
 
-const CoordinatesButton = (props) => {
+const CoordinatesButton = ({onReceiveCoordinates}) => {
 
+    return (
+
+        <button onClick={(e) => onReceiveCoordinates([e.clientX, e.clientY]) }> click me </button>
+    )
 
 }
